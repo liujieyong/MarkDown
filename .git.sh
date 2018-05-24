4d1852b07904a861ab6288bddc8837bc1d05b290
jieyong <jieyongliu@outlook.com>
2018-05-24 16:28:37 +0800

    commit by L at

--- .git.sh
+++ .git.sh
@@ -1,5 +1,6 @@
 #! bin/bash
 cd ~/Documents/GitHub/MarkDown
+DATE=`date+%Y-%m-%d`
 git add .
-git commit -m "commit by L at `date+%Y-%m-%d`"
+git commit -m "commit by L at $DATE"
 git push origin master
